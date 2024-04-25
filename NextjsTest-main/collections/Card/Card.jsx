0eb7ed592eb82ccf6e7d318f5925426cb0362f59
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2024-04-25 17:12:55 +0300

    added text picture and container elements

--- NextjsTest-main/collections/Card/Card.jsx
+++ NextjsTest-main/collections/Card/Card.jsx
@@ -1,16 +1,61 @@
 import Image from "next/image";
+import brief from "../../resources/brief.png"
+import search from "../../resources/search.png"
+import pitch from "../../resources/pitch.png"
 
 import {
     StyledButtonContainer,
-    StyledGetStartedBtn,
+    StyledBtnBrief,
+    StyledBtnSearch,
+    StyledBtnPitch,
+    StyledTextContainer,
+    StyledButtonTitle,
+    StyledButtonText,
+    StyledImageContainer
 } from "./elements";
 
 export const CardButtons = () => {
     return (
         <StyledButtonContainer>
-            <StyledGetStartedBtn>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis similique atque fugiat eaque, ad consequatur! Obcaecati quod aliquam ut esse, nemo</StyledGetStartedBtn>
-            <StyledGetStartedBtn>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, facilis? Voluptates, </StyledGetStartedBtn>
-            <StyledGetStartedBtn>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium totam odit ad soluta in quidem veritatis nisi alias tenetur praesentium</StyledGetStartedBtn>
+            <StyledBtnBrief>
+                <StyledImageContainer>
+                    <Image src={brief} alt="" layout="responsive" />
+                </StyledImageContainer>
+                <StyledTextContainer>
+                    <StyledButtonTitle>
+                        Brief
+                    </StyledButtonTitle>
+                    <StyledButtonText>
+                        Complete <span style={{ "font-weight": '600' }}>brief writing or simple guidance</span> on what to include, we've got you covered.
+                    </StyledButtonText>
+                </StyledTextContainer>
+            </StyledBtnBrief>
+            <StyledBtnSearch>
+                <StyledImageContainer>
+                    <Image src={search} alt="" layout="responsive" />
+                </StyledImageContainer>
+                <StyledTextContainer>
+                    <StyledButtonTitle>
+                        Search
+                    </StyledButtonTitle>
+                    <StyledButtonText>
+                        In-depth agency search covering; <span style={{ "font-weight": '600' }}>criteria matching</span>, door knocking and due-dilligence vetting.
+                    </StyledButtonText>
+                </StyledTextContainer>
+            </StyledBtnSearch>
+            <StyledBtnPitch>
+                <StyledImageContainer>
+                    <Image src={pitch} alt="" layout="responsive" />
+                </StyledImageContainer>
+                <StyledTextContainer>
+                    <StyledButtonTitle>
+                        Pitch
+                    </StyledButtonTitle>
+                    <StyledButtonText>
+                        Comprehensive <span style={{ "font-weight": '600' }}>pitch management</span>, including comms, diary management and pitch hosting.
+                    </StyledButtonText>
+                </StyledTextContainer>
+            </StyledBtnPitch>
         </StyledButtonContainer>
     );
 };
