b8b27c81556c39a2e098b48ec72d2c1148c6f758
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2024-04-27 01:49:45 +0300

    made agency section responsive

--- NextjsTest-main/sections/AgencySelection/elements.jsx
+++ NextjsTest-main/sections/AgencySelection/elements.jsx
@@ -5,23 +5,37 @@ export const StyledContainer = styled(({ height, ...props }) => <SectionContaine
   display: inherit;
 `;
 
-export const StyledTitle = styled((props) => <SectionBigHeading {...props} />)`
-  align-items: center;
-  margin: 0;
-  color: black;
-`;
-
-export const StyledDescription = styled((props) => <SectionSubheading {...props} />)`
-  margin: 5px 0 0 0;
-  font-size: 24px;
-`;
-
 export const StyledTextContainer = styled(({ ...props }) => <div {...props} />)`
   color: black;
   width: 100%;
   padding: 6rem 0 0 0;
   text-align: center;
   font-family: sans-serif;
+
+  @media (max-width: 1024px) {
+    width: 100%;
+    margin: 0 0 5rem 0;
+  }
+`;
+
+export const StyledTitle = styled((props) => <SectionBigHeading {...props} />)`
+  align-items: center;
+  margin: 0;
+  color: black;
+
+  @media (max-width: 1024px) {
+    font-size: 60px;
+    padding: 0 0 10px 0;
+  }
+`;
+
+export const StyledDescription = styled((props) => <SectionSubheading {...props} />)`
+  margin: 5px 0 0 0;
+  font-size: 24px;
+
+  @media (max-width: 1024px) {
+    font-size: 30px;
+  }
 `;
 
 export const StyledCTAContainer = styled(({ ...props }) => <div {...props} />)`
@@ -29,6 +43,12 @@ export const StyledCTAContainer = styled(({ ...props }) => <div {...props} />)`
   justify-content: center;
   padding: 3rem 0 4rem 0;
   position: relative;
+
+  @media (max-width: 1024px) {
+    align-items: center;
+    flex-direction: column;
+    padding: 0;
+  }
 `;
 
 export const StyledVideoContainer = styled(({ ...props }) => <div {...props} />)`
@@ -36,15 +56,36 @@ export const StyledVideoContainer = styled(({ ...props }) => <div {...props} />)
   max-height: 20rem;
   width: 100%;
   height: 100%;
+
+  @media (max-width: 1024px) {
+    max-width: 50vw;
+    max-height: 70vh;
+    width: 100%;
+    height: 100%;
+  }
 `;
 
 export const StyledImageContainer = styled(({ ...props }) => <div {...props} />)`
-  max-width: 60rem;
-  max-height: 20rem;
+  max-width: 45rem;
+  max-height: 31.5rem;
   width: 100%;
   height: 100%;
   position: absolute;
+  top: 55px;
   left: 0;
   margin: 20px 0 0 9.7rem;
-  z-index: -1
+  z-index: -1;
+
+  @media (max-width: 1024px) {
+    position: absolute;
+    top: 40px;
+    left: -100px;
+    max-width: 100%;
+    max-height: 40vh;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    margin: 0;
+    z-index: -1;
+  }
 `;
